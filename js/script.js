6581b3d5b54ce50c2beb37f8bfb90fe0e54e03b9
Emily <ejliu68@gmail.com>
2017-08-30 01:39:09 -0400

    Initial commit; stub pages

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// // stack overflow
+// $(document).on('click', 'a', function(event){
+//   event.preventDefault();
+//   $('html, body').animate({
+//     scrollTop: $($.attr(this, 'href')).offset().top
+//   }, 500);
+// });
