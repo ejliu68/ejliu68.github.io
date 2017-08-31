a397de7925fa0f2077a22786dd24f3f00df260dd
Emily <ejliu68@gmail.com>
2017-08-31 16:38:15 -0400

    Added animations

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-function showDropdown(){
-  document.getElementById('drop-menu').classList.toggle("new-drop-style");
-}
+$(document).ready(function() {
+  // $('.content').mouseover(function() {
+  //   $('.content').css('box-shadow', '10px 10px 5px #888888');
+  // });
+  $('.drop-btn').click(function() {
+      $('.drop-style').toggleClass("new-drop-style");
+  });
+  $('.blogpost').mouseover(function(){
+    // $(this).css('transform', 'translate3d(2%, 0, 0)')
+    $(this).css('transform', 'scale(1.05)')
+    $(this).css('transition-timing-function', 'ease-in')
+    $(this).css('animation-fill-mode', 'both')
+  }).mouseout(function(){
+    // $(this).css('transform', 'translate3d(-2%, 0, 0)')
+    $(this).css('transform', 'scale(1)')
+    $(this).css('transition-timing-function', 'ease-in')
+    $(this).css('animation-fill-mode', 'both')
+  });
+});
+// function showDropdown(){
+//   document.getElementById('drop-menu').classList.toggle("new-drop-style");
+// }
